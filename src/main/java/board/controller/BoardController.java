30becc01091e6a048f805310733255625ba813da
kimjuhyeon <tmxhsk99@naver.com>
2018-10-22 21:50:18 +0900

    no message

--- src/main/java/board/controller/BoardController.java
+++ src/main/java/board/controller/BoardController.java
@@ -1,5 +1,24 @@
 package board.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import booking.bus.bean.BusVO;
+import booking.controller.BookingService;
+
+@Controller
 public class BoardController {
+	@Autowired
+	BookingService bookingService;
+	
+	@RequestMapping(value="/booking/booking_input.do")
+	public ModelAndView booking_input(BusVO vo) {
+		
+		
+		return null;
+		
+	}
 
 }

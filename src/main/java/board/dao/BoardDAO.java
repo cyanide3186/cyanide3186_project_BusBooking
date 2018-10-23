5e23b958f9cdc0400694810a129c00522062fa54
InsooYoon <cyanide3186@gmail.com>
2018-10-23 21:44:26 +0900

    브랜지 머지용

--- src/main/java/board/dao/BoardDAO.java
+++ src/main/java/board/dao/BoardDAO.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -13,16 +13,16 @@ import board.bean.BoardVO;
 @Repository
 public class BoardDAO {
 	@Autowired
-	SqlSession sqlSession;
+	SqlSessionTemplate sqlSessionTemplate;
 	
 	//공지문 입력 (사용 X) 
 	public int insertBoard(BoardVO boardVO) {
-		return sqlSession.insert("mybatis.board-Mapper.insertBoard", boardVO);
+		return sqlSessionTemplate.insert("mybatis.board-Mapper.insertBoard", boardVO);
 	}
 	
 	//공지문 삭제 (사용 X)
 	public int deleteBoard(int board_no) {
-		return sqlSession.delete("mybatis.board-Mapper.deleteBoard", board_no);
+		return sqlSessionTemplate.delete("mybatis.board-Mapper.deleteBoard", board_no);
 	}
 	
 	//공지문 페이징
@@ -30,16 +30,16 @@ public class BoardDAO {
 		Map<String, Integer> map=  new HashMap<>();
 		map.put("startNum", startNum);
 		map.put("endNum", endNum);
-		return sqlSession.selectList("mybatis.board-Mapper.pagingBoard", "map");
+		return sqlSessionTemplate.selectList("mybatis.board-Mapper.pagingBoard", "map");
 	}
 	
 	//공지문 갯수 확인
 	public int countBoard() {
-		return sqlSession.selectOne("mybatis.board-Mapper.countBoard");
+		return sqlSessionTemplate.selectOne("mybatis.board-Mapper.countBoard");
 	}
 	
 	//공지문 확인
 	public BoardVO selectBoard(int board_no) {
-		return sqlSession.selectOne("mybatis.board-Mapper.selectBoard", board_no);
+		return sqlSessionTemplate.selectOne("mybatis.board-Mapper.selectBoard", board_no);
 	}
 }

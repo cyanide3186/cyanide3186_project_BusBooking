2589f02c7601003810ba9b582a8108503d9983e0
JinwooChoi <ctl8710@gmail.com>
2018-10-23 19:51:24 +0900

    업로드 테스트

--- src/main/java/board/dao/BoardDAO.java
+++ src/main/java/board/dao/BoardDAO.java
@@ -12,7 +12,7 @@ import board.bean.BoardVO;
 
 @Repository
 public class BoardDAO {
-	@Autowired
+	/*@Autowired
 	SqlSession sqlSession;
 	
 	//공지문 입력 (사용 X) 
@@ -41,5 +41,5 @@ public class BoardDAO {
 	//공지문 확인
 	public BoardVO selectBoard(int board_no) {
 		return sqlSession.selectOne("mybatis.board-Mapper.selectBoard", board_no);
-	}
+	}*/
 }

a4ce16b11b31edd9dfe9b97268fa9f69b5acdac3
구정록 <ii0_0ii@naver.com>
2018-10-24 20:37:55 +0900

    중복된 DAO 재수정

--- src/main/java/booking/ticket/dao/TicketDAO.java
+++ src/main/java/booking/ticket/dao/TicketDAO.java
@@ -1,24 +1,35 @@
 package booking.ticket.dao;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import booking.bus.bean.BusVO;
 import booking.ticket.bean.TicketVO;
 
 @Repository
 public class TicketDAO {
-	
 	@Autowired
-	SqlSessionTemplate sqlsession;
+	SqlSessionTemplate sqlSession;
 	
-	public int insertTicket(TicketVO vo) {
-		return sqlsession.insert("mybatis.ticket-Mapper.booking", vo);
+	public int booking(TicketVO ticketVO) {
+	    return sqlSession.insert("mybatis.ticket-Mapper.booking", ticketVO);
 	}
 	
-	public int deleteTicket(String ticket_no) {
-		return sqlsession.delete("mybatis.ticket-Mapper.bookingCancel", ticket_no);
+	public int bookingCancel(int ticket_no) {
+		return sqlSession.delete("mybatis.ticket-Mapper.bookingCancel", ticket_no);
+	}
+
+	public TicketVO bookingCheck(int ticket_no) {
+		return sqlSession.selectOne("mybatis.ticket-Mapper.bookingCheck", ticket_no);
 	}
 	
-	public 
+	public int bookingModify(int ticket_no) {
+		return sqlSession.selectOne("mybatis.ticket-Mapper.bookingModify", ticket_no);
+	}
+	
 }

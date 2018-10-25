8e086e71bd7535bdb4fed46b475659043a485ca3
kimjuhyeon <tmxhsk99@naver.com>
2018-10-25 21:30:38 +0900

    기본구조 bus_input

--- src/main/java/booking/controller/BookingController.java
+++ src/main/java/booking/controller/BookingController.java
@@ -7,9 +7,9 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class BookingController {
 	
-	@RequestMapping(value="/booking/booking_bus.do")
+	@RequestMapping(value="/booking/booking_input.do")
 	public ModelAndView booking_bus(ModelAndView modelAndView) {
-		modelAndView.addObject("main","../booking/booking_bus.jsp");
+		modelAndView.addObject("main","../booking/booking_input.jsp");
 		modelAndView.setViewName("../main/index.jsp");
 		return modelAndView;
 	}

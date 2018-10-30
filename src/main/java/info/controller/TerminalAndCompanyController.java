fee6d890c2d63b0fa9daed3b191edb37cae86392
user <chadabin12@gmail.com>
2018-10-30 21:32:58 +0900

    *

--- src/main/java/info/controller/TerminalAndCompanyController.java
+++ src/main/java/info/controller/TerminalAndCompanyController.java
@@ -1,15 +1,8 @@
 package info.controller;
 
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
-import info.compamy.bean.CompanyVO;
 
 @Component
 public class TerminalAndCompanyController {

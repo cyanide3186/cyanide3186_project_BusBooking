377eb511c7e2ed5fd1180843a27991843eedf3ac
InsooYoon <cyanide3186@gmail.com>
2018-10-31 20:33:48 +0900

    -다중 insert 및 중복되는 값 삭제 쿼리문 추가

--- src/main/webapp/sql/terminal.sql
+++ src/main/webapp/sql/terminal.sql
@@ -13,14 +13,6 @@ commit;
 --단일 insert
 insert into terminal values('충청남도','논산시','논산','041-)735-2372','충청남도 논산시 계백로 1000','','');
 
---다중 insert **일별로 입력
-10/30(월)
-insert all          --권역/시군구/명칭/부기정보/주소/홈페이지/약도
-    into terminal values('서울특별시','광진구','동서울','1688-5979','서울 광진구 구의동 546-1','https://www.ti21.co.kr/','') 
-    into terminal values('경기도','가평군','가평','','경기 가평군 가평읍 가화로 51','','')
-    into terminal values('경기도','평택시','평택','','경기 평택시 평택동 185-245','','')
-    into terminal values('충청남도','논산시','논산','041-)735-2372','충청남도 논산시 계백로 1000','','')	
-select *from dual;
 --정보 확인
 select *from terminal;
 --정보 수정
@@ -42,3 +34,15 @@ select * from(select rownum rn, tt.* from (select * from terminal order by name 
 select * from
 (select rownum rn, tt.* from (select * from terminal order by name asc) tt)
  where (region = '경기도' and (name like '%%' or subtext like '%평%')) and (rn>=1 and rn<=5);
+--------------------------------------------------------------------------------------------------
+**아래는 복사붙여넣기 해서 실행하면됩니다.
+--다중 insert
+insert all          --권역/시군구/명칭/부기정보/주소/홈페이지/약도
+    into terminal values('서울특별시','광진구','동서울','1688-5979','서울 광진구 구의동 546-1','https://www.ti21.co.kr/','') 
+    into terminal values('경기도','가평군','가평','','경기 가평군 가평읍 가화로 51','','')
+    into terminal values('경기도','평택시','평택','','경기 평택시 평택동 185-245','','')
+    into terminal values('충청남도','논산시','논산','041-)735-2372','충청남도 논산시 계백로 1000','','')	
+select *from dual;
+
+--명칭이 중복된 터미널 제거
+delete from terminal a where ROWID >(select MIN(ROWID) from terminal b where a.name = b.name);

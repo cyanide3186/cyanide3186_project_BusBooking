cadcc25fde042a549f2e4045f415a1fb129fe7e9
user <chadabin12@gmail.com>
2018-10-31 18:08:13 +0900

    운수사

--- src/main/java/info/controller/TerminalAndCompanyController.java
+++ src/main/java/info/controller/TerminalAndCompanyController.java
@@ -5,28 +5,57 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import info.company.bean.CompanyVO;
 
 
-@Component
+@Controller
 public class TerminalAndCompanyController {
 	/*오토와이어 설정*/
 	@Autowired
 	InfoService infoService;
 	
 	//운수사 리스트 목록
-	@RequestMapping(value="/info/company_do")
+	
+	@RequestMapping(value="/info/company_info.do")
+	public ModelAndView companyList(HttpServletRequest request) {
+		System.out.println("pg="+request.getParameter("pg"));;
+		int pg = Integer.parseInt(request.getParameter("pg"));
+		
+		 int endNum= pg*10;
+	        int startNum= endNum-9;
+	        List<CompanyVO> list = infoService.pagingCompanyBoard(startNum, endNum);
+	        int totalC=infoService.CountCompany();
+	        int totalP=(totalC+9)/10;
+	        
+	        int startPage=(pg-1)/10*10+1;    
+	        int endPage=startPage+9;
+	        if(endPage>totalP) {
+	            endPage=totalP;
+	        } 
+	        
+	        ModelAndView modelAndView = new ModelAndView();
+	        modelAndView.addObject("pg", pg);
+	        modelAndView.addObject("list", list);
+	        modelAndView.addObject("startPage", startPage);
+	        modelAndView.addObject("endPage", endPage);
+	        modelAndView.addObject("totalP", totalP);
+	        modelAndView.addObject("main", "../info/company_info.jsp");
+	        modelAndView.setViewName("../main/index.jsp");
+	        return modelAndView;	        
+	}
+/*	
+	@RequestMapping(value="/info/companyList.do")
 	public ModelAndView ListCompany(HttpServletRequest request) {
-        String str_pg=request.getParameter("pg");
-        int pg=1;
+        int pg = Integer.parseInt(request.getParameter("pg"));
+        //int pg=1;
         if(str_pg!=null){
             pg=Integer.parseInt(str_pg);
         }
-        
+        System.out.println("컴퍼니리스트 두");
         int endNum= pg*10;
         int startNum= endNum-9;
         List<CompanyVO> list = infoService.pagingCompanyBoard(startNum, endNum);
@@ -38,6 +67,7 @@ public class TerminalAndCompanyController {
         if(endPage>totalP) {
             endPage=totalP;
         } 
+        
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("pg", pg);
         modelAndView.addObject("list", list);
@@ -45,9 +75,53 @@ public class TerminalAndCompanyController {
         modelAndView.addObject("endPage", endPage);
         modelAndView.addObject("totalP", totalP);
         
+        
         modelAndView.addObject("main", "../info/terminal_company_info.jsp");
         modelAndView.setViewName("../main/index.jsp");
         return modelAndView;
 		
-	}
+	}*/
+	
+/*	@RequestMapping(value="/info/terminal_company_info_json.do")
+	public ModelAndView terminal_company_info_json(HttpServletRequest request)throws Exception {
+		
+		System.out.println("글 목록 json 처리");
+		int pg = Integer.parseInt(request.getParameter("pg"));
+		System.out.println("pg="+pg);
+		int endNum = pg * 10;
+		int startNum = endNum - 9;
+		
+		List<CompanyVO> list = infoService.pagingCompanyBoard(startNum, endNum);
+		
+		String rt = null;
+		int total = list.size(); 	//조회된 데이터 수
+		if(total > 0) {
+			rt = "OK";
+		}else {
+			rt = "FAIL";
+		}
+		
+		JSONObject json = new JSONObject();
+		json.put("rt", rt);
+		json.put("total", total);
+		
+		if(total < 0) {
+			JSONArray items = new JSONArray();
+			for(int i=0; i<list.size(); i++) {
+				CompanyVO companyVO = list.get(i);
+				JSONObject temp = new JSONObject();
+				temp.put("name", companyVO.getName());
+				temp.put("tel", companyVO.getTel());
+				temp.put("addr", companyVO.getAddr());
+				
+				items.put(i, temp);
+			}
+			json.put("items", items);
+		}
+		System.out.println(json);
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("json", json);
+		modelAndView.setViewName("terminal_company_info_Json.jsp");
+		return modelAndView;
+	}*/
 }
--- src/main/webapp/info/terminal_company_info.jsp
+++ src/main/webapp/info/terminal_company_info.jsp
@@ -6,6 +6,7 @@
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+<script type="text/javascript" src="../js/jquery-3.3.1.min.js"></script>
 <link rel="stylesheet" type="text/css" href="../css/base.css">
 <style type="text/css">
 #logo1:hover {text-decoration: none; background-color: #87CEFA;}
@@ -67,16 +68,19 @@ table#tab1 {
 		});
 	});
 	
-	$(function() {
+/*  	$(function() {
 		$("#logo2").click(function() {	//운수사안내 클릭
 			$("#tab1").hide();		//터미널안내 테이블
 			$("#tab2").fadeIn();	//운수사안내 테이블
 			$(".terminal_table").hide();
 			$(".company_table").fadeIn();
+	
 			
-		});
+		}); 
 		
-	});
+	});*/
+	
+	
 </script>
 </head>
 <body>
@@ -88,8 +92,8 @@ table#tab1 {
 
 	<table id="tab" >
 		<tr>
-			<td id="logo1" width="400" height="80"><a>터미널 안내</a></td>
-			<td id="logo2" width="400"><a href="#">운수사 안내</a></td>
+			<td id="logo1" width="400" height="80"><a href="../info/infoTrAndCompany.do">터미널 안내</a></td>
+			<td id="logo2" width="400"><a href="../info/company_info.do?pg=1">운수사 안내</a></td>
 		</tr>
 	</table>
 		
@@ -157,45 +161,7 @@ table#tab1 {
 		</c:if>
 		</td>
 	</tr>
-	</table>
-	
-		<div id="tab2">
-		<table class="company_table" border="1">
-		<tr height="50">
-			<th width="300">구/회사명</th>
-			<th width="200">전화번호</th>
-			<th width="590">주소</th>
-		</tr>
-	<c:forEach var="companyVO" items="${list}">
-		<tr align="center" height="50" >
-			<td width="300" class="line">${companyVO.name}</td>
-			<td width="200" class="linecenter">${companyVO.tel}</td>
-			<td width="590" class="line">${companyVO.addr}</td>
-		</tr>
-	</c:forEach>	
-    <tr>
-       <td colspan="5" align="center" height="70px;">
-       <c:if test="${startPage > 10 }">
-       <a class="Paging" href="../info/terminal_company_info.do?pg=${startPage -1 }" style="font-size: 25px;">이전</a>
-      </c:if>
-         
-		<c:forEach var="i" begin="${startPage}" end="${endPage}" step="1">
-			<c:if test="${pg == i}">
-				<a id="currentPaging" href="../info/terminal_company_info.do?pg=${i}"><img src=>${i}</a>
-			</c:if>
-			<c:if test="${pg != i}">
-				<a id="paging" href="../info/terminal_company_info.do?pg=${i}">${i}</a>
-			</c:if>		
-		</c:forEach>
-		
-		<c:if test="${endPage < totalP}">
-			<a id="paging" href="../info/terminal_company_info.do?pg=${endPage + 1}" style="font-size: 25px;">다음</a>		
-		</c:if>
-		</td>
-	</tr>
-	</table>
 
-		</div>
 
 	
 </div>
